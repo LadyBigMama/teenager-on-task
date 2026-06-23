6ec3ad3188838aae2e919af449c657801cffd542
LadyBigMama <sandra@kosumi.com>
2026-06-23 13:36:15 -0700

    Publish Supabase config

---
+++ config.js
@@ -0,0 +1,6 @@
+window.SUPABASE_CONFIG = {
+  url: "https://gekjeqcdslurlsrkwmnd.supabase.co",
+  anonKey: "sb_publishable_QsISptCvxtkaHXmV2iQ8rQ_9Lgnt5we",
+  householdId: "reed-household",
+  table: "teen_task_state"
+};
